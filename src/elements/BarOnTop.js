e57ce0832f7562765634da11ac7ef698bbbb52aa
misz0rr <maciejmyszka99@gmail.com>
2021-09-15 14:49:49 +0200

    Update BarOnTop.js

--- src/elements/BarOnTop.js
+++ src/elements/BarOnTop.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navigation from "./Navigation";
 import menu from "../images/menu-icon.svg";
 import profile from "../images/profile-icon.svg";
 import flag from "../images/poland_icon.png";
 
+import AppContext from "../AppContext";
+
 const BarOnTop = () => {
+  const { setCity, setObject } = useContext(AppContext);
+
+  const resetDetails = () => {
+    setCity("");
+    setObject("");
+  };
+
   return (
     <div className="company-wrapper">
       <div className="company-intro">
-        <h3>
-          <a
-            href="/homepage.js"
-            style={{ textDecoration: "none", color: "#fff" }}
-          >
-            Booking<span>.com</span>
-          </a>
+        <h3 onClick={() => resetDetails()}>
+          Booking<span>.com</span>
         </h3>
         <div className="panel-mobile">
           <img src={profile} alt="profil" />
